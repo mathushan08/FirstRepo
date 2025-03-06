9e6b1315a5b6856b022533721cf8542d8378bd87
mathushan08 <mathushanmaheswaran4@gmail.com>
2025-03-06 14:44:00 +0530

    added a fuction for substracting two numbers

--- add.py
+++ add.py
@@ -1,9 +1,12 @@
 # simple script for add two numbers 
 def add_numbers(num1,num2):
     return num1 + num2
-
+def sub_numbers(num1,num2):
+    return num1 - num2
 # test the function
 
 if __name__ == "__main__":
     result = add_numbers(8,6)
+    sub_result = sub_numbers(8,6)
     print(f"The result of the addition is: {result}")
+    print(f"The result of subtraction is: {subtraction_result}")
